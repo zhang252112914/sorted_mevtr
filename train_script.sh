7a5f25f523a0142c2d595a18909684d59fea030b
Roy <2010286495@qq.com>
2024-10-24 07:34:03 +0000

    10.24.2 from server

---
+++ train_script.sh
@@ -0,0 +1,13 @@
+CUDA_VISIBLE_DEVICES=0,1,3,7 torchrun --nproc_per_node=4 \
+--master_port=53198 \
+main.py --do_train \
+--num_thread_reader=2 \
+--data_path /home/zyl/MeVTR_data_and_models/charades/annotation \
+--features_path /home/zyl/MeVTR_data_and_models/charades/Charades_v2_3 \
+--output_dir /home/zyl/sorted_mevtr/output \
+--max_words 77 --max_frames 64 --batch_size_val 16 \
+--datatype charades --feature_framerate 1 --coef_lr 1e-4 \
+--slice_framepos 2 --batch_size 48 \
+--loose_type --linear_patch 2d --sim_header meanP \
+--pretrained_clip_name ViT-B/32 \
+--post_process cluster --post_cluster_centroids 16 
